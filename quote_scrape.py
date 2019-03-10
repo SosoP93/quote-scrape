96e536ba798dcac9b1f6e3f3770a7fa19cc7ab1a
SosoP93 <34781353+SosoP93@users.noreply.github.com>
2019-03-10 12:33:42 -0700

    quote_scrape.py V1.2
    Capable of scraping qoutes on all pages on "http://quotes.toscrape.com"

--- quote_scrape.py
+++ quote_scrape.py
@@ -1,22 +1,26 @@
-#Work in progress
-
 import requests
 from bs4 import BeautifulSoup
 from csv import reader, writer
 
+
+
 def soup_request(url):
 	""" Get html of website and turn into BeautifulSoup object
-	so that we ca navigate html for information"""
+	so that we can navigate html for information"""
 	r = requests.get(url)
 	
 	soup = BeautifulSoup(r.text, "html.parser")
 	return soup
 
 	
+
 def scraper(soup):
-	""" scrape information from html and gather into a list"""
+	""" scrape information from html and add into a list"""
+
+	# target 'class = quote' in order to gather quote content
 	quotes = soup.select(".quote")
 
+	# empty list for adding in the lists of quotes
 	info = []
 
 	for quote in quotes:
@@ -29,21 +33,27 @@ def scraper(soup):
 		info.append([author_name, content, author_born, author_bplace])
 	return info
 
+
+
 def write_csv(contents):
 	""" Create new file and write content in it"""
-	with open("quotes.csv", "w") as csv_file:
+	with open("quotes.csv", "w", encoding = "utf-8") as csv_file:
 		csv_write = writer(csv_file)
 
 		for content in contents:
 			csv_write.writerow(content)
 
+
+
 def append_csv(contents):
 	""" Open existing file and add content to it"""
-	with open("quotes.csv", "a") as csv_file:
+	with open("quotes.csv", "a", encoding = "utf-8") as csv_file:
 		csv_append = writer(csv_file)
 
 		for content in contents:
 			csv_append.writerow(content)
+
+
 
 # url that will be scraped
 url = "http://quotes.toscrape.com"
@@ -51,13 +61,25 @@ main_page = soup_request(url)
 contents = scraper(main_page)
 write_csv(contents)
 
-# testing scraping process for multiple pages
-page_num = 2
-next_url = url + f"/page/{page_num}"
-next_page = soup_request(next_url)
-info = scraper(next_page)
-append_csv(info)
-# test_for_next = next_page.select(".next")[0]
+
+# While a class called 'next' exists in the html, this loop will
+# scrape the page, append rows to the csv file, and then
+# move to next page
+page_num = 1
+
+test = True
+while test:
+	page_num += 1	
+	next_page_url = url + f"/page/{page_num}"
+	next_page = soup_request(next_page_url)
+	info = scraper(next_page)
+	append_csv(info)
+
+	if not next_page.select(".next"):
+		test = False
+
+
+	
 
 
 
